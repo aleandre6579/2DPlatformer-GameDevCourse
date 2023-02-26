3586dbb942b2f20ce33c173fc88326476602baad
aleandre6579 <aleandre6579.simon@gmail.com>
2023-02-26 23:28:02 +0700

    Made a main menu

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private GameObject playerRef;
+
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPosition = playerRef.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, smoothTime);
     }
 }
